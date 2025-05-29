53e3fc621892b7de94369972e50a44d0f9ed95aa
yezixx <skylow917@daum.net>
2025-05-29 21:50:31 +0900

    refactor: member 엔티티 통합

--- src/main/java/Sookmyung/Lingo/domain/Member.java
+++ src/main/java/Sookmyung/Lingo/domain/Member.java
@@ -2,6 +2,8 @@ package Sookmyung.Lingo.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class Member {
     @Id
@@ -17,14 +19,20 @@ public class Member {
     @Column(nullable = false)
     private String password;
 
+    // 회원 이름
+    @Column(name = "member_name", nullable = false)
+    private String name;
+
+    // 생년월일
+    @Column(nullable = false)
+    private Date birth;
+
+    // 전화번호
+    @Column(nullable = false)
+    private String phone_num;
+
     // 회원 유형
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private MemberType memberType;
-
-    // === 관계 설정 ===
-    @OneToOne(mappedBy = "member",
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL)
-    private GeneralMember generalMember;
 }
