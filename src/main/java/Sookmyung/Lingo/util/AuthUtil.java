29787f831e83ebb337c01f68848b4fe0fe4bf6ff
yezixx <skylow917@daum.net>
2025-06-25 10:00:53 +0900

    feat: 로그인 사용자 정보를 조회하는 AuthUtil 유틸 클래스 추가
    - SecurityContext에서 인증된 사용자의 이메일을 추출
    - 이메일을 통해 현재 로그인된 Member 엔티티 조회
    - 서비스 및 컨트롤러에서 로그인 사용자 정보를 간편하게 사용 가능

---
+++ src/main/java/Sookmyung/Lingo/util/AuthUtil.java
@@ -0,0 +1,23 @@
+package Sookmyung.Lingo.util;
+
+import Sookmyung.Lingo.domain.Member;
+import Sookmyung.Lingo.exception.MemberNotFoundException;
+import Sookmyung.Lingo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthUtil {
+    private final MemberRepository memberRepository;
+
+    public Member getCurrentMember() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = ((User) auth.getPrincipal()).getUsername();
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new MemberNotFoundException());
+    }
+}
