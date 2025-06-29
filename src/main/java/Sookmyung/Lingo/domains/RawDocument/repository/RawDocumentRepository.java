5fb75564ca0c4c1daae0244f7acd140747b38e43
shinyj0 <yejins0605@gmail.com>
2025-06-30 00:31:13 +0900

    feat: RawDocumentRepository 구현

--- src/main/java/Sookmyung/Lingo/domains/RawDocument/repository/RawDocumentRepository.java
+++ src/main/java/Sookmyung/Lingo/domains/RawDocument/repository/RawDocumentRepository.java
@@ -1,4 +1,10 @@
 package Sookmyung.Lingo.domains.RawDocument.repository;
 
-public class RawDocumentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Sookmyung.Lingo.domains.RawDocument.domain.RawDocument;
+
+@Repository
+public interface RawDocumentRepository extends JpaRepository<RawDocument, Long> {
 }
