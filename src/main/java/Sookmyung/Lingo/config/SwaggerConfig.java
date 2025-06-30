05a19e2d9a7521812dff1b305481038f08d23bb9
yezixx <skylow917@daum.net>
2025-06-30 11:54:16 +0900

    feat: Swagger 설정에 JWT 인증을 위한 보안 스키마 추가
    - OpenAPI 설정에 SecurityScheme 및 SecurityRequirement 추가
    - JWT 인증 테스트를 위해 Swagger UI에 Authorize 버튼 노출되도록 구성
    - 잘못된 위치에 사용된 addSecurityItem() 호출을 OpenAPI 객체로 이동하여 오류 수정

--- src/main/java/Sookmyung/Lingo/config/SwaggerConfig.java
+++ src/main/java/Sookmyung/Lingo/config/SwaggerConfig.java
@@ -1,25 +1,31 @@
 package Sookmyung.Lingo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-	@Bean
-	public OpenAPI openAPI() {
-		return new OpenAPI()
-			.components(new Components())
-			.info(apiInfo());
-	}
+    private static final String SECURITY_SCHEME_NAME = "Authorization";
 
-	private Info apiInfo() {
-		return new Info()
-			.title("LINGO api 명세서")
-			.description("LINGO api 명세서입니다.")
-			.version("1.0.0");
-	}
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("LINGO api 명세서")
+                        .description("LINGO api 명세서입니다.")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
 }
