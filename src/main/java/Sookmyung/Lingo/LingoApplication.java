df9b521e7ce866d7b2b614f8e57ce2e9dabaa8b4
shinyj0 <yejins0605@gmail.com>
2025-07-04 20:45:00 +0900

    feat: S3Config 구현

--- src/main/java/Sookmyung/Lingo/LingoApplication.java
+++ src/main/java/Sookmyung/Lingo/LingoApplication.java
@@ -9,17 +9,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class LingoApplication {
 
 	public static void main(String[] args) {
+
 		// .env 파일 로드
 		Dotenv dotenv = Dotenv.configure()
-			.directory("./") // .env 파일 경로 설정 (기본: 프로젝트 루트)
 			.load();
-
 		// 환경변수를 시스템 프로퍼티에 추가
 		dotenv.entries().forEach(entry ->
 			System.setProperty(entry.getKey(), entry.getValue())
 		);
 
 		SpringApplication.run(LingoApplication.class, args);
+
 	}
 
 }
