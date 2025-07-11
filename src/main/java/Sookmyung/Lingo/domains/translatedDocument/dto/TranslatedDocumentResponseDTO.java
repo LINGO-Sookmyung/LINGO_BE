4252fc932d1ea9f4d4f948aa26dbfd20be4582ed
shinyj0 <yejins0605@gmail.com>
2025-07-11 17:30:00 +0900

    feat:TranslatedDocumentResponseDTO 구현

--- src/main/java/Sookmyung/Lingo/domains/translatedDocument/dto/TranslatedDocumentResponseDTO.java
+++ src/main/java/Sookmyung/Lingo/domains/translatedDocument/dto/TranslatedDocumentResponseDTO.java
@@ -1,4 +1,36 @@
 package Sookmyung.Lingo.domains.translatedDocument.dto;
 
+import Sookmyung.Lingo.domains.enums.Country;
+import Sookmyung.Lingo.domains.enums.DocumentType;
+import Sookmyung.Lingo.domains.rawDocument.domain.RawDocument;
+import Sookmyung.Lingo.domains.translatedDocument.domain.TranslatedDocument;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class TranslatedDocumentResponseDTO {
+
+	private Long rawDocumentId;
+	private DocumentType documentType;
+	private Country country;
+
+	private Long translatedDocumentId;
+	private String translatedDocumentName;
+	private String translatedFileUrl;
+
+	public static TranslatedDocumentResponseDTO from(RawDocument rawDocument) {
+		TranslatedDocument translated = rawDocument.getTranslatedDocument();
+
+		return TranslatedDocumentResponseDTO.builder()
+			.rawDocumentId(rawDocument.getId())
+			.documentType(rawDocument.getDocumentType())
+			.country(rawDocument.getCountry())
+			.translatedDocumentId(translated.getId())
+			.translatedDocumentName(translated.getTranslatedDocumentName())
+			.translatedFileUrl(translated.getTranslatedFilePath())
+			.build();
+	}
 }
