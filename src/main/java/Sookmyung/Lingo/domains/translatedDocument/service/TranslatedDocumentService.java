852f2c9aca708659207ffa7620629c421572897d
shinyj0 <yejins0605@gmail.com>
2025-07-12 23:19:49 +0900

    refactor: presignedDownloadUrl 함께 반환하도록 수정

--- src/main/java/Sookmyung/Lingo/domains/translatedDocument/service/TranslatedDocumentService.java
+++ src/main/java/Sookmyung/Lingo/domains/translatedDocument/service/TranslatedDocumentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import Sookmyung.Lingo.domains.rawDocument.repository.RawDocumentRepository;
+import Sookmyung.Lingo.domains.s3.service.S3Service;
 import Sookmyung.Lingo.domains.translatedDocument.domain.TranslatedDocument;
 import Sookmyung.Lingo.domains.translatedDocument.dto.TranslatedDocumentResponseDTO;
 import Sookmyung.Lingo.domains.translatedDocument.repository.TranslatedDocumentRepository;
@@ -15,25 +16,31 @@ import Sookmyung.Lingo.domains.translatedDocument.repository.TranslatedDocumentR
 public class TranslatedDocumentService {
 
 	private final TranslatedDocumentRepository translatedDocumentRepository;
+	private final S3Service s3Service;
 
 	@Autowired
-	public TranslatedDocumentService(TranslatedDocumentRepository translatedDocumentRepository) {
+	public TranslatedDocumentService(TranslatedDocumentRepository translatedDocumentRepository, S3Service s3Service) {
 		this.translatedDocumentRepository = translatedDocumentRepository;
+		this.s3Service =s3Service;
 	}
 
 	public List<TranslatedDocumentResponseDTO> getTranslatedDocumentsByMemberId(Long memberId) {
+
 		List<TranslatedDocument> documents = translatedDocumentRepository.findAllByRawDocument_Member_Id(memberId);
 
 		return documents.stream()
 			.map(translated -> {
 				var raw = translated.getRawDocument();
+				String s3Key = translated.getTranslatedFilePath();
+				String presignedDownloadUrl = s3Service.getPresignedUrlToDownload(s3Key).getPath();
 				return TranslatedDocumentResponseDTO.builder()
 					.rawDocumentId(raw.getId())
 					.documentType(raw.getDocumentType())
 					.country(raw.getCountry())
 					.translatedDocumentId(translated.getId())
 					.translatedDocumentName(translated.getTranslatedDocumentName())
-					.translatedFileUrl(translated.getTranslatedFilePath())
+					.translatedFilePath(translated.getTranslatedFilePath())
+					.presignedDownloadUrl(presignedDownloadUrl)
 					.build();
 			})
 			.toList();
